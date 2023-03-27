fac4ecf200cef0f2e17325218e7cd37a40931923
Maria Ilieva <123466748+marianilieva@users.noreply.github.com>
2023-03-27 13:55:59 +0300

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+export default function Header()    {
+    return (
+        <div className='header'>
+            <h1 className='title'>
+                Update Ourselves
+            </h1>
+            <p>
+                Life changes everyday. People become smarter, opportunities
+                grow.
+            </p>
+        </div>
+    )
+}
