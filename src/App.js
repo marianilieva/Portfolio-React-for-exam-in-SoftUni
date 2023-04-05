5d806c7e7a7207e5658b5942e9be4a0319c16e6b
Maria Ilieva <123466748+marianilieva@users.noreply.github.com>
2023-04-05 14:53:09 +0300

    Third update with all dependencies

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import  Header  from './components/Header/Header';
 import Dependencies from './components/Dependencies/Dependencies';
+
 import AllDependencies from './components/AllDependencies/AllDependencies';
 import Physical from './components/AllDependencies/Physical/Physical';
 import Mind from './components/AllDependencies/Mind/Mind';
+import Geographical from './components/AllDependencies/Geographical/Geographical';
+import Social from './components/AllDependencies/Social/Social';
+import Interests from './components/AllDependencies/Interests/Interests';
+import Error from './components/AllDependencies/Error/Error';
+import Login from './components/UserInteractions/Login/Login';
+
 import Footer from './components/Footer/Footer';
 import Menu from './components/Menu/Menu';
 
@@ -23,10 +30,11 @@ function App() {
           </>} />
           <Route path='/physical' element={<Physical />} />
           <Route path='/mind' element={<Mind />} />
-          <Route path='/' element={<div />} />
-          <Route path='/' element={<div />} />
-          <Route path='/' element={<div />} />
-          <Route path='/' element={<div />} />
+          <Route path='/geographical' element={<Geographical />} />
+          <Route path='/social' element={<Social />} />
+          <Route path='/interests' element={<Interests />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Error />} />
         </Routes>
 
         <Footer />
